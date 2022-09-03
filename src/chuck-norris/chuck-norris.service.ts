ffb2306b37f753c75d526b216d9bdf0809d065de
davissbf <davissbf@gmail.com>
2022-09-02 23:35:05 -0300

    [UPT] Adicionando CRUD da API do Chuck Norris.

--- src/chuck-norris/chuck-norris.service.ts
+++ src/chuck-norris/chuck-norris.service.ts
@@ -3,8 +3,9 @@ import axios from 'axios';
 import { Repository } from 'typeorm';
 import { ChuckNorrisEntity } from './chuck-norris.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { IOutputChuckNorris } from './interfaces/OutputChuckNorris';
 import { environment } from '../config/environment';
+import { IApiChuckNorris } from './interfaces/ApiChuckNorris.interface';
+import { IUpdateChuckNorris } from './interfaces/UpdateChuckNorris.interface';
 
 @Injectable()
 export class ChuckNorrisService {
@@ -15,7 +16,7 @@ export class ChuckNorrisService {
     private readonly chuckNorrisRepository: Repository<ChuckNorrisEntity>,
   ) {}
 
-  async listAll(): Promise<ChuckNorrisEntity> {
+  async apiChuckNorris(): Promise<ChuckNorrisEntity> {
     this.logger.debug('Consumindo a API do Chuck Norris...');
 
     try {
@@ -23,7 +24,7 @@ export class ChuckNorrisService {
 
       const result = await axios.get(endpoint);
 
-      return this.saveDataInEntityChuckNorris(result.data);
+      return this.formatSaveChuckNorris(result.data);
     } catch (error) {
       this.logger.error(`
         ERRO no MS "${environment.app.name}", método "listAll".
@@ -42,20 +43,30 @@ export class ChuckNorrisService {
     }
   }
 
+  formatSaveChuckNorris(dataChuckNorris: IApiChuckNorris): ChuckNorrisEntity {
+    this.logger.debug('Formatando os dados do Chuck Norris...');
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return {
+      categorias: dataChuckNorris.categories,
+      iconeUrl: dataChuckNorris.icon_url,
+      idApiChuckNorris: dataChuckNorris.id,
+      url: dataChuckNorris.url,
+      valor: dataChuckNorris.value,
+    };
+  }
+
   async saveDataInEntityChuckNorris(
-    data: IOutputChuckNorris,
+    inputChuckNorris: IApiChuckNorris,
   ): Promise<ChuckNorrisEntity> {
     this.logger.debug('Salvando dados na entidade ChuckNorris');
 
     try {
       return this.chuckNorrisRepository.save(
-        this.chuckNorrisRepository.create({
-          categorias: data.categories,
-          iconeUrl: data.icon_url,
-          idApiChuckNorris: data.id,
-          url: data.url,
-          valor: data.value,
-        }),
+        this.chuckNorrisRepository.create(
+          this.formatSaveChuckNorris(inputChuckNorris),
+        ),
       );
     } catch (error) {
       this.logger.error(`
@@ -65,7 +76,202 @@ export class ChuckNorrisService {
         <'ERRO'>
           MESSAGE: Erro ao salvar dados na entidade do chuck norris...
         Parâmetros da requisição:
-          BODY: ${JSON.stringify(data)}
+          BODY: ${JSON.stringify(inputChuckNorris)}
+        Resposta:
+        <'ERRO SQL'>
+          CODE: ${Number(error.code) || HttpStatus.INTERNAL_SERVER_ERROR}
+          MESSAGE: ${error.message}.
+      `);
+
+      throw new HttpException(
+        error.message,
+        Number(error.code) || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async findAndSave(): Promise<ChuckNorrisEntity> {
+    this.logger.debug('Buscando e salvando dados da API do Chuck Norris.');
+
+    try {
+      const chuckNorris = await this.apiChuckNorris();
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const objChuckNorris: IApiChuckNorris = {
+        categories: chuckNorris.categorias,
+        icon_url: chuckNorris.iconeUrl,
+        id: chuckNorris.idApiChuckNorris,
+        url: chuckNorris.url,
+        value: chuckNorris.valor,
+      };
+
+      return this.saveDataInEntityChuckNorris(objChuckNorris);
+    } catch (error) {
+      this.logger.error(`
+        ERRO no MS "${environment.app.name}", método "findAndSave".
+        <'ERRO'>
+          MESSAGE: Erro ao buscar e salvar a API do chuck norris...
+        Resposta:
+        <'ERRO SQL'>
+          CODE: ${Number(error.code) || HttpStatus.INTERNAL_SERVER_ERROR}
+          MESSAGE: ${error.message}.
+      `);
+
+      throw new HttpException(
+        error.message,
+        Number(error.code) || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async findAll(): Promise<Array<ChuckNorrisEntity>> {
+    this.logger.debug('Buscando todos os Chuck Norris.');
+
+    try {
+      return this.chuckNorrisRepository.find();
+    } catch (error) {
+      this.logger.error(`
+        ERRO no MS "${environment.app.name}", método "findAll".
+        <'ERRO'>
+          MESSAGE: Erro ao buscar todos os Chuck Norris na base de dados...
+        Resposta:
+        <'ERRO SQL'>
+          CODE: ${Number(error.code) || HttpStatus.INTERNAL_SERVER_ERROR}
+          MESSAGE: ${error.message}.
+      `);
+
+      throw new HttpException(
+        error.message,
+        Number(error.code) || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async findById(idChuckNorris: string): Promise<ChuckNorrisEntity> {
+    this.logger.debug(`Buscando Chuck Norris de ${idChuckNorris}.`);
+
+    try {
+      const chuckNorris = await this.chuckNorrisRepository.findOne({
+        where: {
+          id: idChuckNorris,
+        },
+      });
+
+      if (chuckNorris) return chuckNorris;
+
+      const erroMenssage = `Não foi encontrado nenhum Chuck Norris com id: ${idChuckNorris} na base de dados...`;
+
+      this.logger.error(`
+        ERRO no MS "${environment.app.name}", método "findById".
+        <'ERRO'>
+          MESSAGE: ${erroMenssage}
+        Resposta:
+        <'ERRO NEGOCIAL'>
+          CODE: ${HttpStatus.INTERNAL_SERVER_ERROR}
+          MESSAGE: ${erroMenssage}
+      `);
+
+      throw new HttpException(erroMenssage, HttpStatus.INTERNAL_SERVER_ERROR);
+    } catch (error) {
+      this.logger.error(`
+        ERRO no MS "${environment.app.name}", método "findById".
+        <'ERRO'>
+          MESSAGE: Erro ao buscar o Chuck Norris de id: ${idChuckNorris} na base de dados...
+        Resposta:
+        <'ERRO SQL'>
+          CODE: ${Number(error.code) || HttpStatus.INTERNAL_SERVER_ERROR}
+          MESSAGE: ${error.message}.
+      `);
+
+      throw new HttpException(
+        error.message,
+        Number(error.code) || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async saveUpdatedChuckNorris(
+    inputChuckNorris: IApiChuckNorris,
+  ): Promise<ChuckNorrisEntity> {
+    this.logger.debug('Salvando dados na entidade ChuckNorris');
+
+    try {
+      await this.chuckNorrisRepository.update(
+        inputChuckNorris.id,
+        inputChuckNorris,
+      );
+
+      return this.findById(inputChuckNorris.id);
+    } catch (error) {
+      this.logger.error(`
+        ERRO no MS "${environment.app.name}", método "saveUpdatedChuckNorris".
+        <'ERRO'>
+          MESSAGE: Erro ao atualizar o chuck norris de id: ${
+            inputChuckNorris.id
+          } na base de dados...
+        Parâmetros da requisição:
+          BODY: ${JSON.stringify(inputChuckNorris)}
+        Resposta:
+        <'ERRO SQL'>
+          CODE: ${Number(error.code) || HttpStatus.INTERNAL_SERVER_ERROR}
+          MESSAGE: ${error.message}.
+      `);
+
+      throw new HttpException(
+        error.message,
+        Number(error.code) || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async update(
+    bodyChuckNorris: IUpdateChuckNorris,
+  ): Promise<ChuckNorrisEntity> {
+    this.logger.debug(`Atualizando Chuck Norris de ${bodyChuckNorris.id}.`);
+
+    try {
+      let chuckNorris = await this.chuckNorrisRepository.findOne({
+        where: {
+          id: bodyChuckNorris.id,
+        },
+      });
+
+      if (chuckNorris) {
+        chuckNorris = {
+          ...chuckNorris,
+          categorias: bodyChuckNorris.categories,
+          iconeUrl: bodyChuckNorris.icon_url,
+          url: bodyChuckNorris.url,
+          valor: bodyChuckNorris.value,
+          tsEdicao: new Date(),
+        };
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        return this.saveUpdatedChuckNorris(chuckNorris);
+      }
+
+      const erroMenssage = `Não foi encontrado nenhum Chuck Norris com id: ${bodyChuckNorris.id} para atualizar na base de dados...`;
+
+      this.logger.error(`
+        ERRO no MS "${environment.app.name}", método "findById".
+        <'ERRO'>
+          MESSAGE: ${erroMenssage}
+        Resposta:
+        <'ERRO NEGOCIAL'>
+          CODE: ${HttpStatus.INTERNAL_SERVER_ERROR}
+          MESSAGE: ${erroMenssage}
+      `);
+
+      throw new HttpException(erroMenssage, HttpStatus.INTERNAL_SERVER_ERROR);
+    } catch (error) {
+      this.logger.error(`
+        ERRO no MS "${environment.app.name}", método "update".
+        <'ERRO'>
+          MESSAGE: Erro ao buscar o Chuck Norris de id: ${
+            bodyChuckNorris.id
+          } na base de dados...
         Resposta:
         <'ERRO SQL'>
           CODE: ${Number(error.code) || HttpStatus.INTERNAL_SERVER_ERROR}
