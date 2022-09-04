02666a6919a4f98dabeb4fef5b3a0806220f99fd
Emanuell Santos de Jesus <dfmanu06@gmail.com>
2022-09-04 20:31:18 -0300

    [UPT] Ajustando swagger

--- src/person/person.ts
+++ src/person/person.ts
@@ -4,6 +4,7 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class Person {
   @PrimaryGeneratedColumn('uuid')
@@ -21,9 +22,19 @@ export class Person {
   @Column({ nullable: false })
   idade: number;
 
+  @ApiProperty({
+    description:
+      'Campo responsável por armazenar o tempo de criação do registro',
+    example: '2022-09-04 22:30:55.539000',
+  })
   @CreateDateColumn({ name: 'timestamp_criacao' })
   tsCriacao: Date;
 
+  @ApiProperty({
+    description:
+      'Campo responsável por armazenar o tempo de edição do registro',
+    example: '2022-09-04 22:30:55.539000',
+  })
   @UpdateDateColumn({ name: 'timestamp_edicao' })
   tsEdicao: Date;
 }
