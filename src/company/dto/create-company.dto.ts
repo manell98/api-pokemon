62137edbabe3b475e6af468218c45f1c00f20544
Emanuell Santos de Jesus <dfmanu06@gmail.com>
2022-09-04 18:17:17 -0300

    [UPT] Ajustando eslint

--- src/company/dto/create-company.dto.ts
+++ src/company/dto/create-company.dto.ts
@@ -1,6 +1,6 @@
-import {CreateAddressDto} from "../../address/dto/create-address.dto";
+import { CreateAddressDto } from '../../address/dto/create-address.dto';
 
 export class CreateCompanyDto {
-    name: string
-    adress: Array<CreateAddressDto>
+  name: string;
+  adress: Array<CreateAddressDto>;
 }
